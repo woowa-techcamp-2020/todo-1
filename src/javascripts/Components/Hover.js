1f707ca255560331e1bb02e4bc75a3c55e32b93e
changicho <changicho29@gmail.com>
2020-07-16 15:40:37 +0900

    [#13] feat: 추가 - Hover component
    노트를 드래그 했을 때 마우스 커서를 따라올 hover 컴포넌트 추가
    
    related issue: #13

---
+++ src/javascripts/Components/Hover.js
@@ -0,0 +1,43 @@
+import Element from './Element.js';
+
+export default class Hover extends Element {
+  constructor() {
+    super();
+    this.target = undefined;
+    this.setElement();
+  }
+
+  changeInnerDom(dom, x, y) {
+    this.target = dom;
+    this.element.appendChild(this.target);
+
+    this.element.style.left = x - this.element.offsetWidth / 2 + 'px';
+    this.element.style.top = y - this.element.offsetHeight / 2 + 'px';
+  }
+
+  clearInnerDom() {
+    if (this.target) {
+      this.element.removeChild(this.target);
+    }
+    this.target = undefined;
+  }
+
+  tracking(x, y) {
+    this.element.style.left = x - this.element.offsetWidth / 2 + 'px';
+    this.element.style.top = y - this.element.offsetHeight / 2 + 'px';
+  }
+
+  consoleLog() {
+    console.log('hello');
+  }
+
+  setElement() {
+    const hover = document.createElement('div');
+    hover.className = 'hover';
+    hover.style.position = 'absolute';
+    hover.style.zIndex = 1000;
+    hover.style.width = 400 + 'px';
+
+    this.element = hover;
+  }
+}
