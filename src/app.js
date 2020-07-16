b625c7499cef4b3cd4069745931f041654e22e4d
changicho <changicho29@gmail.com>
2020-07-16 10:52:39 +0900

    [#3] feat: 적용 - 칸반보드 페이지 컴포넌트 적용
    칸반보드 페이지에 앞서 작성한 컴포넌트 렌더링 적용
    index.html의 #app에 해당 컴포넌트를 렌더링하도록 설정
    
    이는 나중에 로그인 페이지가 추가 될 경우 분기처리 시 #app에 컴포넌트를
    갈아끼우는 방식으로 구현하려고 하기 위함
    
    related issue: #3
    related commit: b5aa482f8e28bd0d500ea6db509600fd9a691554

--- src/app.js
+++ src/app.js
@@ -2,3 +2,11 @@ import '@babel/polyfill';
 
 // eslint-disable-next-line no-unused-vars
 import style from './stylesheets/style.css';
+
+import KanbanPage from './javascripts/Components/KanbanPage.js';
+
+const app = document.querySelector('#app');
+
+const kanbanPage = new KanbanPage().render();
+
+app.appendChild(kanbanPage);
