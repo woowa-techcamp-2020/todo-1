5e2f3f18783b69ea6ac4f28a585fd08f575804e1
changicho <changicho29@gmail.com>
2020-07-21 11:08:05 +0900

    [#14] feat: 추가 - 노트 생성 DAO method
    새로운 노트를 생성하는 DAO method 추가
    순서는 다음과 같음
    - 칼럼의 맨 뒤에 있는 노트의 id를 가져옴
    - 그 노트 뒤에 새로운 노트를 추가함 (이전에 받은 id값으로 연결구조 생성)
    - 이전에 맨 뒤에 있던 노트의 연결구조를 update함
    
    쿼리를 3번 수행하므로 중간에 하나라도 문제가 생겼을 때 rollback 하기
    위해서 transaction을 적용할 예정
    이는 mysql2/promise를 이용해야 하므로 다음 commit에서 DAO의 리팩토링이
    들어감
    
    related issue: #14

---
+++ dao/method/createNote.js
@@ -0,0 +1,80 @@
+const safePromise = require('../../utils/safePromise');
+
+const Note = require('../dto/Note');
+
+const SELECT_LAST_NOTE = `SELECT id from NOTE
+WHERE column_id = ? AND next_note_id is null;`;
+
+const INSERT_NOTE = `INSERT INTO NOTE 
+(column_id, user_id, \`content\`, prev_note_id, next_note_id)
+VALUES (?, ?, ?, ?, null);
+`;
+
+const UPDATE_LAST_NOTE = `UPDATE NOTE
+SET next_note_id = ?
+WHERE id = ?;`;
+
+module.exports = async function setNote(columnId, noteData) {
+  const [connection, connectionError] = await safePromise(this.getConnection());
+  if (connectionError) {
+    throw connectionError;
+  }
+  let result = false;
+
+  try {
+    // await connection.beginTransaction();
+
+    // GET LAST NOTE OF THIS COLUMN
+    // ?: column_id
+    const [lastNoteRow, lastNoteRowError] = await safePromise(
+      this.executeQuery(connection, SELECT_LAST_NOTE, [columnId]),
+    );
+
+    if (lastNoteRow.length === 0 || lastNoteRowError) {
+      throw new Error();
+    }
+
+    const lastNoteId = lastNoteRow[0].id;
+
+    // INSERT NOTE
+    // ?: column_id, user_id, content, prev_note_id
+    const [noteRow, noteRowError] = await safePromise(
+      this.executeQuery(connection, INSERT_NOTE, [
+        columnId,
+        noteData.userId,
+        noteData.content,
+        lastNoteId,
+      ]),
+    );
+
+    if (noteRowError) {
+      throw new Error();
+    }
+    const { insertId } = noteRow;
+
+    // UPDATE NOTE
+    // ?: next_note_id, id
+    const [updateRow, updateRowError] = await safePromise(
+      this.executeQuery(connection, UPDATE_LAST_NOTE, [insertId, lastNoteId]),
+    );
+
+    if (updateRowError) {
+      throw new Error();
+    }
+
+    result = new Note(
+      insertId,
+      noteData.content,
+      noteData.user,
+      noteData.userId,
+    );
+
+    // await connection.commit();
+  } catch (error) {
+    // connection.rollback();
+  } finally {
+    connection.release();
+  }
+
+  return result;
+};
