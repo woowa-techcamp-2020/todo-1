eb9a20096820102af0d5d129a259aabb5612e684
changicho <changicho29@gmail.com>
2020-07-22 15:53:11 +0900

    [#53] feat: 추가 - 연결 구조의 유효성 검사
    노트 이동 시, 도착한 위치의 연결 관계가 유효한지 검사하는 safe guard
    추가
    
    입력받은 id에 해당하는 row를 검색 후, 연결 관계를 검사하는 방식으로 구현
    row[0], row[1]로 두 개의 결과를 받은 경우
    - row[0] > row[1]
    - row[1] > row[0]
    두 가지 경우 조사

--- dao/method/moveNote.js
+++ dao/method/moveNote.js
@@ -1,5 +1,8 @@
 const safePromise = require('../../utils/safePromise');
 
+const READ_LINK_NOTES = `SELECT id, prev_note_id, next_note_id FROM NOTE
+WHERE id = ? OR id = ?`;
+
 const READ_NOTE_LINK = `SELECT prev_note_id, next_note_id FROM NOTE
 WHERE id = ?;`;
 
@@ -15,6 +18,40 @@ const UPDATE_LINK = `UPDATE NOTE
 SET prev_note_id = ?, next_note_id = ?
 WHERE id = ?;`;
 
+/**
+ * 입력받은 인자의 유효성 검사
+ * @param {*} connection  mysql2 connection 객체
+ * @param {*} query       query를 실행하는 함수
+ * @param {*} beforeId    연결 구조에서 앞에 위치한 노트
+ * @param {*} afterId     연결 구조에서 뒤에 위치한 노트
+ */
+async function checkCorrectLink(connection, query, beforeId, afterId) {
+  if (!beforeId || !afterId) {
+    return true;
+  }
+
+  // READ LINK NOTES
+  // prev_note_id, next_note_id
+  const [rows, error] = await safePromise(
+    query(connection, READ_LINK_NOTES, [beforeId, afterId]),
+  );
+  if (error || rows.length !== 2) {
+    return false;
+  }
+
+  const isValidLink =
+    (rows[0].next_note_id === rows[1].id &&
+      rows[1].prev_note_id === rows[0].id) ||
+    (rows[0].prev_note_id === rows[1].id &&
+      rows[1].next_note_id === rows[0].id);
+
+  // 연결 관계가 유효한지 check
+  if (isValidLink) {
+    return true;
+  }
+  return false;
+}
+
 module.exports = async function moveNote(noteId, beforeNoteId, afterNoteId) {
   const [connection, connectionError] = await safePromise(this.getConnection());
   if (connectionError) {
@@ -25,6 +62,16 @@ module.exports = async function moveNote(noteId, beforeNoteId, afterNoteId) {
   try {
     await connection.beginTransaction();
     let rows, error;
+
+    const isCorrectLink = await checkCorrectLink(
+      connection,
+      this.executeQuery,
+      beforeNoteId,
+      afterNoteId,
+    );
+    if (!isCorrectLink) {
+      throw new Error();
+    }
 
     /**
      * 이전 노트와의 연결 관계를 끊어줌
@@ -95,7 +142,7 @@ module.exports = async function moveNote(noteId, beforeNoteId, afterNoteId) {
       ]),
     );
 
-    // 하나의 note만 update하지 않은경우
+    // 에러가 발생했거나, 하나의 note만 update하지 않은경우
     if (error || rows.affectedRows !== 1) {
       throw new Error();
     }
