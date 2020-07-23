dfe1acfcabf992884a6d32a600cd96adfa7e36bd
changicho <changicho29@gmail.com>
2020-07-23 18:50:06 +0900

    [#8] feat: 추가 - 액티비티 로그 호출 api
    인자로 page를 넘겨주면 해당 page까지의 log를 전달해주는 api 제작

--- apis/index.js
+++ apis/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const loginRouter = require('./login');
 const kanbanRouter = require('./kanban');
+const logRouter = require('./log');
 
 const router = express.Router();
 
 router.use(loginRouter);
 router.use(kanbanRouter);
+router.use(logRouter);
 
 module.exports = router;
