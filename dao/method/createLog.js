3202a9df71486341ec930101ccceff4c5af7b741
changicho <changicho29@gmail.com>
2020-07-23 16:44:26 +0900

    [#32] feat: 변경 - 변경된 schema 구조에 맞춰서 log 부분 수정
    createLog 메소드를 호출 시 인자로 넣어줘야 하는 값들을 변경함.
    
    database schema 구조를 변경하는 이유
    - noteId 등을 외래키로 설정하거나, 그렇게 title을 받아오는 경우,
    이후에 내용이 바뀌었을 때, 이전 기록의 내용들도 title이 전부 바뀜
    - 따라서 log가 생성될 때의 기록만 나타내도록 구성

--- dao/method/createLog.js
+++ dao/method/createLog.js
@@ -4,7 +4,15 @@ const Log = require('../dto/Log');
 const CONSTANT = require('../constants/log');
 
 const INSERT_LOG = `INSERT INTO ACTIVITY_LOG 
-(method, \`type\`, note_id, \`column\`, column_to, content_before, content_after)
+(
+  \`method\`,
+  \`type\`,
+  user_name,
+  note_title,
+  column_title,
+  column_to_title,
+  change_title
+)
 VALUES (?, ?, ?, ?, ?, ?, ?);`;
 
 function trimUpperCase(string) {
@@ -22,82 +30,74 @@ function trimUpperCase(string) {
   return null;
 }
 
-function trimNumber(string) {
-  const number = parseInt(string);
-  if (isNaN(number)) {
-    return null;
-  }
-  return number;
-}
-
 /**
  * 객체들의 type을 명확히 해주는 함수
- * @param {Object} data { type, method, ?noteId, ?columnId, ?columnTo, ?contentBefore, ?contentAfter }
+ * @param {Object} data
  * @returns {Object} data 가공한 데이터
  */
 function trimData({
   type,
   method,
-  noteId,
-  columnId,
-  columnTo,
-  contentBefore,
-  contentAfter,
+  userName,
+  noteTitle,
+  columnTitle,
+  columnToTitle,
+  changeTitle,
 }) {
   const data = {};
 
   data.method = trimUpperCase(method);
   data.type = trimUpperCase(type);
-  data.noteId = trimNumber(noteId);
-  data.columnId = trimNumber(columnId);
-  data.columnTo = trimNumber(columnTo);
-  data.contentBefore = contentBefore ? `${contentBefore}` : null;
-  data.contentAfter = contentAfter ? `${contentAfter}` : null;
+  data.userName = userName ? `${userName}` : null;
+  data.noteTitle = noteTitle ? `${noteTitle}` : null;
+  data.columnTitle = columnTitle ? `${columnTitle}` : null;
+  data.columnToTitle = columnToTitle ? `${columnToTitle}` : null;
+  data.changeTitle = changeTitle ? `${changeTitle}` : null;
 
   return data;
 }
 
 function checkParams({
-  type,
   method,
-  noteId,
-  columnId,
-  columnTo,
-  contentBefore,
-  contentAfter,
+  type,
+  userName,
+  noteTitle,
+  columnTitle,
+  columnToTitle,
+  changeTitle,
 }) {
   switch (type) {
     case CONSTANT.TYPE.COLUMN: {
-      if (method === CONSTANT.METHOD.CREATE && (!columnId || !contentAfter)) {
+      if (method === CONSTANT.METHOD.CREATE && (!userName || !columnTitle)) {
         throw new Error('컬럼 생성 인자 부족');
       }
-      if (method === CONSTANT.METHOD.DELETE && !columnId) {
+      if (method === CONSTANT.METHOD.DELETE && (!userName || !columnTitle)) {
         throw new Error('컬럼 삭제 인자 부족');
       }
       if (
         method === CONSTANT.METHOD.MODIFY &&
-        (!columnId || !contentAfter || !contentBefore)
+        (!userName || !columnTitle || !changeTitle)
       ) {
-        throw new Error('컬럼 삭제 인자 부족');
+        throw new Error('컬럼 수정 인자 부족');
       }
       break;
     }
     case CONSTANT.TYPE.NOTE: {
-      if (method === CONSTANT.METHOD.CREATE && (!noteId || !contentAfter)) {
+      if (method === CONSTANT.METHOD.CREATE && (!userName || !noteTitle)) {
         throw new Error('노트 생성 인자 부족');
       }
-      if (method === CONSTANT.METHOD.DELETE && !noteId) {
+      if (method === CONSTANT.METHOD.DELETE && (!userName || !noteTitle)) {
         throw new Error('노트 삭제 인자 부족');
       }
       if (
         method === CONSTANT.METHOD.MODIFY &&
-        (!noteId || !contentAfter || !contentBefore)
+        (!userName || !noteTitle || !changeTitle)
       ) {
         throw new Error('노트 변경 인자 부족');
       }
       if (
         method === CONSTANT.METHOD.MOVE &&
-        (!noteId || !columnId || !columnTo)
+        (!userName || !noteTitle || !columnTitle || !columnToTitle)
       ) {
         throw new Error('노트 이동 인자 부족');
       }
@@ -108,45 +108,48 @@ function checkParams({
 
 /**
  * 로그를 생성하는 method
+ * @param {Object} connection  mysql2 connection 객체
+ * @param {Function} query     query를 실행하는 함수
  * @param {Object} data { type, method, ?noteId, ?columnId, ?columnTo, ?contentBefore, ?contentAfter }
  */
 module.exports = async function createLog(data) {
+  data = trimData(data);
   const {
-    type,
     method,
-    noteId,
-    columnId,
-    columnTo,
-    contentBefore,
-    contentAfter,
-  } = trimData(data);
+    type,
+    userName,
+    noteTitle,
+    columnTitle,
+    columnToTitle,
+    changeTitle,
+  } = data;
 
   if (!method || !type) {
     return false;
   }
-
   const [connection, connectionError] = await safePromise(this.getConnection());
   if (connectionError) {
     throw connectionError;
   }
-  let result = false;
 
+  let result = false;
   try {
     await connection.beginTransaction();
     // 각 method, type 별로 prametors가 제대로 있는지 검사
+
     checkParams(data);
 
     // INSERT_LOG
-    // ?: method, type, note_id, column, column_to, content_before, content_after
+    // ?: method, type, user_name, note_title, column_title, column_to_title, change_title
     const [row, rowError] = await safePromise(
       this.executeQuery(connection, INSERT_LOG, [
         method,
         type,
-        noteId,
-        columnId,
-        columnTo,
-        contentBefore,
-        contentAfter,
+        userName,
+        noteTitle,
+        columnTitle,
+        columnToTitle,
+        changeTitle,
       ]),
     );
 
